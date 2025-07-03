33853d1de13ec37cba60b468c5531b1484e522a6
jsm-Investika <jjsm3826@gmail.com>
2025-07-03 17:47:25 +0300

    Create App.js

---
+++ public/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{textAlign: 'center', marginTop: '50px'}}>
+      <h1>Karibu INVESTIKA</h1>
+      <p>Mfumo wa kidigitali wa uwekaji na uwekezaji wa pesa.</p>
+    </div>
+  );
+}
+
+export default App
